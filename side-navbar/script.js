6fd0b4e1904a29f9012e89c0ef4485060cd3a712
AudumabrKhare <audumbarkhare1112@gmail.com>
2023-03-29 22:54:37 +0530

    change menu name and remove scroll the body content from html and javascript

--- side-navbar/script.js
+++ side-navbar/script.js
@@ -4,7 +4,7 @@ const btnOpen = document.getElementById('btn-open');
 const main = document.getElementById('main');
 var isopen = true;
 
-main.style.marginLeft = "250px";
+// main.style.marginLeft = "250px";
 console.log(btnClose);
 btnClose.addEventListener('click', () => {
     sidebar.style.display = 'none';
@@ -15,10 +15,10 @@ btnOpen.addEventListener('click', () => {
     isopen = !isopen;
     if (isopen) {
         sidebar.style.display = 'block';
-        main.style.marginLeft = "250px";
+        // main.style.marginLeft = "250px";
     } else {
         sidebar.style.display = 'none';
-        main.style.marginLeft = "0";
+        // main.style.marginLeft = "0";
     }
 
 })
